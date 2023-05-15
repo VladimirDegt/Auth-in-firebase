7e0fc024f31156afb414514044d3c2cd6fbb640e
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-05-15 12:03:03 +0300

    google

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { onClickSignUp } from "./js/auth-listeners/on-click-sign-up";
 import { onClickSignIn } from "./js/auth-listeners/on-click-sign-in";
 import { onClickLogOut } from "./js/auth-listeners/on-click-log-out";
+import { onClickGoogle } from "./js/auth-listeners/on-click-google";
 
 document.querySelector('#signUp').addEventListener('click', onClickSignUp);
 document.querySelector('#signIn').addEventListener('click', onClickSignIn);
 document.querySelector('#logOut').addEventListener('click', onClickLogOut);
+document.querySelector('#google').addEventListener('click', onClickGoogle);
 
 
 
